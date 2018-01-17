fc62006b2eaf524ea8a8321e4f75fec589f36480
Vishwa Raj <qwertyasvishwa@outlook.com>
2018-01-17 06:28:04 +0530

    chganges

--- loginSystem/app.js
+++ loginSystem/app.js
@@ -26,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/select', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
