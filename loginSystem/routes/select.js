66b2a853e48a11eb4e4c81b4c2bb3af18bbc5b80
Vishwa Raj <qwertyasvishwa@outlook.com>
2018-01-17 14:09:17 +0530

    ccbhdf

--- loginSystem/routes/select.js
+++ loginSystem/routes/select.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-
+    console.log('select route');
     var mysql = require('mysql');
     var mydata = "";
     var con = mysql.createConnection({
